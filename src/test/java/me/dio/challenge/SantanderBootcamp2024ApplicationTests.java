c3c1362f010f5882a0d75a594b811bfbffb85abf
Fasboyr <fabio.filho2002@hotmail.com>
2024-06-19 13:27:52 -0300

    first commit

---
+++ src/test/java/me/dio/challenge/SantanderBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
